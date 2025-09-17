c544677c296cb4f8030e21957a7fcd4b0640fbca
Md Sohail <talk2sohail100@gmail.com>
2025-09-17 13:49:39 +0530

    fix index test failure

--- index.go
+++ index.go
@@ -19,7 +19,6 @@ type index struct {
 	size uint64
 }
 
-
 func newIndex(f *os.File, c Config) (*index, error) {
 	idx := &index{
 		file: f,
@@ -45,7 +44,6 @@ func newIndex(f *os.File, c Config) (*index, error) {
 	return idx, nil
 }
 
-
 func (i *index) Close() error {
 	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
 		return err
@@ -53,12 +51,15 @@ func (i *index) Close() error {
 	if err := i.file.Sync(); err != nil {
 		return err
 	}
+	if err := i.mmap.UnsafeUnmap(); err != nil {
+		return err
+	}
+
 	if err := i.file.Truncate(int64(i.size)); err != nil {
 		return err
 	}
 	return i.file.Close()
 }
-
 
 func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
 	if i.size == 0 {
@@ -78,7 +79,6 @@ func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
 	return out, pos, nil
 }
 
-
 func (i *index) Write(off uint32, pos uint64) error {
 	if uint64(len(i.mmap)) < i.size+entWidth {
 		return io.EOF
@@ -88,7 +88,6 @@ func (i *index) Write(off uint32, pos uint64) error {
 	i.size += uint64(entWidth)
 	return nil
 }
-
 
 func (i *index) Name() string {
 	return i.file.Name()
